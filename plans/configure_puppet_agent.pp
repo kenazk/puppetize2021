22a91cddc39f125f4a1f82c72a0ba62773a3f068
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-09-02 13:52:29 -0700

    no sudo

--- plans/configure_puppet_agent.pp
+++ plans/configure_puppet_agent.pp
@@ -2,7 +2,7 @@ plan puppetize::configure_puppet_agent(
   String[0] $puppetserver,
   TargetSpec $target
 ){
-  run_command('sudo curl -k https://ip-172-31-19-181.us-west-1.compute.internal:8140/packages/current/install.bash | sudo bash)
+  run_command('curl -k https://ip-172-31-19-181.us-west-1.compute.internal:8140/packages/current/install.bash | bash)
 #  run_command("sudo puppet config set server ${puppetserver}", $target)
-  run_command('sudo puppet agent -t', $target)
+  run_command('puppet agent -t', $target)
 }
