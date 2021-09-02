7af85e0a6d2d9cb31dfc7a0bde4a22fc0eec853d
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-09-02 14:26:41 -0700

    removed target refs

--- plans/configure_puppet_agent.pp
+++ plans/configure_puppet_agent.pp
@@ -1,6 +1,5 @@
-plan puppetize::configure_puppet_agent(
-  TargetSpec $target
-){
+plan puppetize::configure_puppet_agent
+{
   run_command('curl -k https://ip-172-31-19-181.us-west-1.compute.internal:8140/packages/current/install.bash | bash')
-  run_command('puppet agent -t', $target)
+  run_command('puppet agent -t')
 }
