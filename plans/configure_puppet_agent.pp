0ee695db1a6278e73aeb263cada0dc6b9035269b
John "The Party Llama" Laffey <48102585+Jlaffey-Puppet@users.noreply.github.com>
2021-09-02 14:23:09 -0700

    asfsdfsd

--- plans/configure_puppet_agent.pp
+++ plans/configure_puppet_agent.pp
@@ -1,5 +1,4 @@
 plan puppetize::configure_puppet_agent(
-  String[0] $puppetserver,
   TargetSpec $target
 ){
   run_command('curl -k https://ip-172-31-19-181.us-west-1.compute.internal:8140/packages/current/install.bash | bash')
